0a506152422a68193e86a21e142c5c9c02265225
PRIYANKA <PRIYANKA@LAPTOP-JH1AORIL>
2025-09-30 21:27:12 +0530

    Commit3

--- GrocceryApplication/src/main/java/utilities/ExcelUtility.java
+++ GrocceryApplication/src/main/java/utilities/ExcelUtility.java
@@ -15,7 +15,7 @@ public class ExcelUtility {
 	static XSSFSheet sh;
 
 	public static String readStringData(int row, int col,String sheet) throws IOException {
-		f = new FileInputStream("C:\\Users\\PRIYANKA\\eclipse-workspace\\GrocceryApplication\\src\\test\\resources\\TestData.xlsx");
+		f = new FileInputStream("C:\\Users\\PRIYANKA\\git\\GroceryApplication\\GrocceryApplication\\src\\test\\resources\\TestData.xlsx");
 		w = new XSSFWorkbook(f);
 		sh = w.getSheet(sheet);
 		XSSFRow r = sh.getRow(row);
@@ -24,7 +24,7 @@ public class ExcelUtility {
 
 	}
 	public static String readIntegerData(int row, int col,String sheet) throws IOException {
-		f = new FileInputStream("C:\\Users\\PRIYANKA\\eclipse-workspace\\GrocceryApplication\\src\\test\\resources\\TestData.xlsx");
+		f = new FileInputStream("C:\\Users\\PRIYANKA\\git\\GroceryApplication\\GrocceryApplication\\src\\test\\resources\\TestData.xlsx");
 		w = new XSSFWorkbook(f);
 		sh = w.getSheet(sheet);
 		XSSFRow r = sh.getRow(row);
